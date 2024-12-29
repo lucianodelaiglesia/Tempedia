9ea25cb4763a09ddb54b23e141bba8bc566061a6
Luciano de la Iglesia <l.delaiglesia@hotmail.com>
2024-12-29 02:13:10 -0300

    Added search bar

--- app/src/main/java/com/ldelaiglesia/tempedia/presentation/temtem_list/TemtemListViewModel.kt
+++ app/src/main/java/com/ldelaiglesia/tempedia/presentation/temtem_list/TemtemListViewModel.kt
@@ -5,10 +5,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ldelaiglesia.tempedia.common.Resource
+import com.ldelaiglesia.tempedia.domain.models.Temtem
 import com.ldelaiglesia.tempedia.domain.usecases.GetTemtemUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -18,6 +20,8 @@ class TemtemListViewModel @Inject constructor(
     private val _state = mutableStateOf(TemtemListState())
     val state: State<TemtemListState> = _state
 
+    private var originalTemtemList = emptyList<Temtem>()
+
     init {
         getTemtemList()
     }
@@ -25,13 +29,29 @@ class TemtemListViewModel @Inject constructor(
     private fun getTemtemList() {
         useCase().onEach { result ->
             when (result) {
-                is Resource.Success -> _state.value =
-                    TemtemListState(temtemList = result.data ?: emptyList())
+                is Resource.Success -> {
+                    originalTemtemList = result.data ?: emptyList()
+                    _state.value =
+                        TemtemListState(temtemList = originalTemtemList)
+                }
 
                 is Resource.Loading -> _state.value = TemtemListState(isLoading = true)
                 is Resource.Error -> _state.value =
-                    TemtemListState(error = result.message ?: "Error desconocido")
+                    TemtemListState(error = result.message ?: "Unknown error")
             }
         }.launchIn(viewModelScope)
+    }
+
+    fun searchTemtem(query: String) {
+        viewModelScope.launch {
+            val filteredList = if (query.isBlank()) {
+                originalTemtemList
+            } else {
+                originalTemtemList.filter { temtem ->
+                    temtem.name.contains(query, ignoreCase = true)
+                }
+            }
+            _state.value = state.value.copy(temtemList = filteredList)
+        }
     }
 }
--- app/src/main/java/com/ldelaiglesia/tempedia/presentation/temtem_list/components/TemtemListScreen.kt
+++ app/src/main/java/com/ldelaiglesia/tempedia/presentation/temtem_list/components/TemtemListScreen.kt
@@ -1,6 +1,7 @@
 package com.ldelaiglesia.tempedia.presentation.temtem_list.components
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -9,7 +10,12 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
@@ -25,35 +31,51 @@ fun TemtemListScreen(
     viewModel: TemtemListViewModel = hiltViewModel()
 ) {
     val state = viewModel.state.value
-    Box(
+    var searchText by remember { mutableStateOf("") }
+    Column(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(top = 20.dp)
+            .fillMaxWidth()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        LazyColumn(
-            modifier = Modifier.fillMaxSize()
+        TextField(
+            value = searchText,
+            onValueChange = {
+                searchText = it
+                viewModel.searchTemtem(it)
+            },
+            label = { Text("Search") },
+            modifier = Modifier.padding(top = 12.dp, bottom = 12.dp)
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
         ) {
-            items(state.temtemList) { temtem ->
-                TemtemListItem(
-                    temtem = temtem,
-                    onItemClick =
-                    { navController.navigate(Screen.TemtemDetailScreen.route + "/${temtem.number}") }
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                items(state.temtemList) { temtem ->
+                    TemtemListItem(
+                        temtem = temtem,
+                        onItemClick =
+                        { navController.navigate(Screen.TemtemDetailScreen.route + "/${temtem.number}") }
+                    )
+                }
+            }
+            if (state.error.isNotBlank()) {
+                Text(
+                    text = state.error,
+                    color = MaterialTheme.colorScheme.error,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp)
+                        .align(Alignment.Center)
                 )
             }
-        }
-        if (state.error.isNotBlank()) {
-            Text(
-                text = state.error,
-                color = MaterialTheme.colorScheme.error,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 20.dp)
-                    .align(Alignment.Center)
-            )
-        }
-        if (state.isLoading) {
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            if (state.isLoading) {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            }
         }
     }
 }
