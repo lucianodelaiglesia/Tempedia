4cc1cd3a0696f811368b450ba82331cba2fa56eb
Luciano de la Iglesia <l.delaiglesia@hotmail.com>
2025-01-04 23:07:03 -0300

    Fixed type list to scroll when necessary

--- app/src/main/java/com/ldelaiglesia/tempedia/presentation/temtem_list/components/FilterMenuContent.kt
+++ app/src/main/java/com/ldelaiglesia/tempedia/presentation/temtem_list/components/FilterMenuContent.kt
@@ -3,8 +3,7 @@ package com.ldelaiglesia.tempedia.presentation.temtem_list.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,6 +11,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
@@ -19,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
@@ -32,55 +33,62 @@ fun FilterMenuContent(
     onTypeSelected: (String, Boolean) -> Unit,
     typeList: List<Type>
 ) {
-    Column(
+    LazyColumn(
         modifier = Modifier
             .fillMaxHeight()
             .background(DarkGray)
             .requiredWidth(250.dp)
     ) {
-        Box(
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(top = 10.dp, bottom = 25.dp)
-        ) {
-            Text(
-                text = "Filter by type",
-                fontSize = 28.sp,
-                color = Color.White,
-            )
-        }
-        typeList.forEach { type ->
+        item {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .clickable {
-                        onTypeSelected(type.name, !selectedTypes.contains(type.name))
-                    },
-                verticalAlignment = Alignment.CenterVertically
+                    .padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
             ) {
-                Checkbox(
-                    modifier = Modifier.size(56.dp),
-                    checked = selectedTypes.contains(type.name),
-                    onCheckedChange = { isChecked ->
-                        onTypeSelected(type.name, isChecked)
-                    }
-                )
                 Text(
-                    text = type.name,
-                    fontSize = 20.sp,
+                    text = "Filter by type",
+                    fontSize = 28.sp,
                     color = Color.White,
-                )
-                Image(
-                    painter = rememberAsyncImagePainter(model = type.icon),
-                    contentDescription = type.name,
-                    modifier = Modifier.size(48.dp)
+                    textAlign = TextAlign.Center,
                 )
             }
-            HorizontalDivider(
-                modifier = Modifier
-                    .height(1.dp),
-                color = ColorPrimary
-            )
+        }
+        item {
+            typeList.forEach { type ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable {
+                            onTypeSelected(type.name, !selectedTypes.contains(type.name))
+                        },
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Checkbox(
+                        modifier = Modifier.size(56.dp),
+                        checked = selectedTypes.contains(type.name),
+                        onCheckedChange = { isChecked ->
+                            onTypeSelected(type.name, isChecked)
+                        }
+                    )
+                    Text(
+                        text = type.name,
+                        fontSize = 20.sp,
+                        color = Color.White,
+                    )
+                    Image(
+                        painter = rememberAsyncImagePainter(model = type.icon),
+                        contentDescription = type.name,
+                        modifier = Modifier.size(48.dp)
+                    )
+                }
+                HorizontalDivider(
+                    modifier = Modifier
+                        .height(1.dp),
+                    color = ColorPrimary
+                )
+            }
         }
     }
 }
