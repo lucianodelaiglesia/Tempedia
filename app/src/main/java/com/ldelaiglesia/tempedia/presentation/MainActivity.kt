f0b380fb126b51f40e018de80761ad5b93f9754a
Luciano de la Iglesia <l.delaiglesia@hotmail.com>
2025-01-07 00:45:35 -0300

    Forcing dark mode

--- app/src/main/java/com/ldelaiglesia/tempedia/presentation/MainActivity.kt
+++ app/src/main/java/com/ldelaiglesia/tempedia/presentation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.MaterialTheme
@@ -23,6 +24,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         setContent {
             TempediaTheme {
                 Surface(
