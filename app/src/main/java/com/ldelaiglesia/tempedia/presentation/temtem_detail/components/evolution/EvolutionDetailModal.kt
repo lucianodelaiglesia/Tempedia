2b0ed31a465282e70baa4ee5c4c405acf2d4efb1
Luciano de la Iglesia <l.delaiglesia@hotmail.com>
2025-02-19 22:08:35 -0300

    Changes on evolution modal

--- app/src/main/java/com/ldelaiglesia/tempedia/presentation/temtem_detail/components/evolution/EvolutionDetailModal.kt
+++ app/src/main/java/com/ldelaiglesia/tempedia/presentation/temtem_detail/components/evolution/EvolutionDetailModal.kt
@@ -1,11 +1,13 @@
 package com.ldelaiglesia.tempedia.presentation.temtem_detail.components.evolution
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
@@ -13,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ldelaiglesia.tempedia.domain.models.EvolutionDetail
@@ -33,68 +36,108 @@ fun EvolutionDetailModal(
                             .fillMaxWidth()
                             .padding(10.dp)
                     ) {
-                        Text(
-                            text = evolution.name,
-                            fontSize = 32.sp,
-                            color = Color.White
-                        )
-                        Box(modifier = Modifier.fillMaxWidth()) {
-                            Row(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(10.dp)
-                                    .align(Alignment.Center),
-                                verticalAlignment = Alignment.CenterVertically
-                            ) {
+                        Box(
+                            modifier = Modifier.fillMaxWidth()
+                        ){
+                            Text(
+                                text = evolution.name,
+                                fontSize = 32.sp,
+                                color = Color.White,
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                            Text(
+                                text = "#${evolution.number}",
+                                modifier = Modifier.wrapContentSize()
+                                    .align(Alignment.TopEnd),
+                                fontSize = 20.sp,
+                                color = Color.LightGray,
+                            )
+                        }
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(10.dp),
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            if (evolution.type == "levels") {
+                                Text(
+                                    text = "Evolution type: leveling ${evolution.level}",
+                                    fontSize = 24.sp,
+                                    color = Color.White
+                                )
+                            } else {
                                 Text(
                                     text = "Evolution type: ${evolution.type}",
                                     fontSize = 24.sp,
-                                    color = Color.White,
-                                    modifier = Modifier.weight(1f)
+                                    color = Color.White
                                 )
-                            }
-                        }
-                        Text(
-                            text = "#${evolution.number}",
-                            fontSize = 24.sp,
-                            color = Color.White
-                        )
-                        Column {
-                            Text(
-                                text = "Traits",
-                                fontSize = 20.sp,
-                                color = Color.White
-                            )
-                            evolution.traits.forEach { trait ->
                                 Text(
-                                    text = trait,
-                                    fontSize = 20.sp,
+                                    text = "How: \n${evolution.description}",
+                                    fontSize = 24.sp,
                                     color = Color.White
                                 )
                             }
                         }
-                        Text(
-                            text = "Evolves at level:${evolution.level}",
-                            fontSize = 24.sp,
-                            color = Color.White
-                        )
-                        Text(
-                            text = "Evolves at trading: ${if (evolution.trading) "Yes" else "No"}",
-                            fontSize = 24.sp,
-                            color = Color.White
-                        )
-                        Column {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(10.dp),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.Start
+                        ) {
                             Text(
-                                text = "Trait Mapping",
+                                text = "Traits:",
                                 fontSize = 24.sp,
                                 color = Color.White
                             )
-                            evolution.traitMapping.forEach { (trait, value) ->
-                                Text(
-                                    text = "$trait -> $value",
-                                    fontSize = 20.sp,
-                                    color = Color.White
-                                )
+                            Row(
+                                modifier = Modifier.fillMaxWidth()
+                                    .padding(4.dp),
+                                horizontalArrangement = Arrangement.SpaceAround,
+                                verticalAlignment = Alignment.CenterVertically
+                            ){
+                                evolution.traits.forEach { trait ->
+                                    Box(modifier = Modifier.weight(1f)) {
+                                        Text(
+                                            text = trait,
+                                            fontSize = 20.sp,
+                                            color = Color.White,
+                                            modifier = Modifier.align(Alignment.Center),
+                                            textAlign = TextAlign.Center
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(10.dp),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.Start
+                        ) {
+                            Text(
+                                text = "Changes to:",
+                                fontSize = 24.sp,
+                                color = Color.White
+                            )
+                            Row(
+                                modifier = Modifier.fillMaxWidth()
+                                    .padding(4.dp),
+                                horizontalArrangement = Arrangement.Center,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                evolution.traitMapping.forEach { (_, value) ->
+                                    Box(modifier = Modifier.weight(1f)) {
+                                        Text(
+                                            text = value,
+                                            fontSize = 20.sp,
+                                            color = Color.White,
+                                            modifier = Modifier.align(Alignment.Center),
+                                            textAlign = TextAlign.Center
+                                        )
+                                    }
+                                }
                             }
                         }
                     }
